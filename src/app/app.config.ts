77a03fa1e030e8cc3f0d086866d07afe46b05c10
[ignaciocaff] <[jigcaffaratti@gmail.com]>
2018-03-09 12:44:23 -0300

    Correcion URI DEV ENV

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
 export class AppConfig {
-     public readonly apiUrl = 'http://localhost/api/';
-     public readonly imgUrl = 'http://localhost';
+     public readonly apiUrl = 'http://localhost:50042/api/';
+     public readonly imgUrl = 'http://localhost:50042';
 };
